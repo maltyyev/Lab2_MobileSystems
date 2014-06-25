f6b1c3540173518a461c22d0e6d5cf2f152c8a36
maltyyev <amk0794@gmail.com>
2014-06-25 19:51:49 +0300

    modified temperature

--- temperature.sh
+++ temperature.sh
@@ -3,11 +3,11 @@
 while true
 do
 	file="page.html"
-	url="http://www.gismeteo.by/weather-minsk-4248/"
+	url="http://www.pogoda.by/"
 	log="log.txt"
 	wget -o log $url -O $file
 	htmlfile=$(<page.html)
-	grep -m 1 "<dd class='value m_temp c'>" $file > temp
+	grep -m 1 "<td width='*' class='synoptic_T'>" $file > temp
 	echo $(sed 's/[^\-\+0-9]//g' temp)
 	sleep $(cat time)
 done
