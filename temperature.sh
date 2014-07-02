4f6c8189cf2d2f8d78c2947a0090bb94e23d1329
maltyyev <amk0794@gmail.com>
2014-07-02 18:49:21 +0300

    temperature

--- temperature.sh
+++ temperature.sh
@@ -3,12 +3,11 @@
 while true
 do
 	file="page.html"
-	url="http://www.meteo.by/"
+	url="http://www.gismeteo.by/"
 	log="log.txt"
 	wget -o log $url -O $file
 	htmlfile=$(<page.html)
-	grep '<p class="t ">' $file > temp
+	grep -m 1 "<dd class='value m_temp c'>" $file > temp
 	echo $(sed 's/[^\-\+0-9]//g' temp)
 	sleep $(cat time)
 done
-
