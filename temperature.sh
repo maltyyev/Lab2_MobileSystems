30dbfc558aee068666acc25f80a781715cec1cd4
maltyyev <amk0794@gmail.com>
2014-07-03 20:10:00 +0300

    modified temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -5,6 +5,6 @@ do
 	wget -o log "http://www.gismeteo.by/" -O page.html
 	grep -m 1 "<dd class='value m_temp c'>" page.html > temp
 	echo $(sed 's/[^\-\+0-9]//g' temp)
-	rm -r log page.html temp
+	rm log page.html temp
 	sleep $(cat time)
 done
